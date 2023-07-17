3e41d6d0bf408854b7f6ff0e3cd69467bb65ca1e
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-07-17 18:23:56 -0400

    fix tailwind css no effect error

--- src/SimpleCollection/tailwind.config.js
+++ src/SimpleCollection/tailwind.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 module.exports = {
-    content: ['../*.{js,jsx}', '../**/*.js'],
+    content: ['../*.{js,jsx}', '../**/*.js', path.join(path.dirname(require.resolve('@uniwebcms/express')), '**/*.js')],
     theme: {},
-    plugins: [require('@tailwindcss/line-clamp')],
+    plugins: [require('@tailwindcss/line-clamp')]
 };
