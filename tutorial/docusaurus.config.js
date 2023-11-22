45778ccb5517dbe6bdbb65d908b704bd6fe4222c
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-11-22 15:36:34 -0500

    fix

--- tutorial/docusaurus.config.js
+++ tutorial/docusaurus.config.js
@@ -3,8 +3,8 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const getConfig = require('@uniwebcms/tutorial-builder/config');
 
+dotenv.config({ path: '../.env.dev' });
 dotenv.config({ path: '../.env' });
-// dotenv.config({ path: '../.env.local' });   // enable this line if you want to have a local .env file
 
 const themeConfig = {
     prism: {
